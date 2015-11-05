311f8103ea3c4a16c882b86786e8e8f2b0f798c3
Lisette <Lisette@Lisette-pc>
2015-11-05 02:34:36 -0300

    Funciones Secretaria

--- Vista/Menus/Secretaria.php
+++ Vista/Menus/Secretaria.php
@@ -1,10 +1,10 @@
 <ul class="nav">
-    <li><a href="">Reservas</a></li>
-    <li><a href="">Clientes</a></li>
-    <li><a href="">Mascotas</a></li>
-    <li><a href="">Insumos</a></li>
+    <li><a href="../Layout/administrarReservaHostal.php">Reservas</a></li>
+    <li><a href="../Layout/administrarPersonas.php">Clientes</a></li>
+    <li><a href="../Layout/administrarMascotas.php">Mascotas</a></li>
+    <!--<li><a href="">Insumos</a></li>
     <li><a href="">Generar Reporte</a></li>
-    <li><a href="">Editar perfil</a></li>
+    <li><a href="">Editar perfil</a></li>-->
     <li><a href="../Servlet/loginOFF.php">Salir</a></li>
 </ul>
 
